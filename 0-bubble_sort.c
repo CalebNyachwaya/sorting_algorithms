568c4e52bf68fb295d791455676ba70c03706ad9
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-06-27 15:06:24 -0700

    Add bubble sort files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+/**
+ * swap1 - swaps tow value
+ *
+ * @x: first value
+ * @y: second value
+ */
+
+void swap1(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using
+ *               the Bubble sort algorithm.
+ *
+ * @array: A pointer to the array of integers to sort.
+ * @size: The number of elements in the array.
+ */
+
+
+void bubble_sort(int *array, size_t size)
+{
+	int flag;
+	size_t i;
+
+	/* Verify if array and size exist */
+	if (!array || !size)
+	return;
+
+	flag = 1;
+	for (i = 0; flag == 1; i++) /* iterate until no swaps are made */
+	{
+		flag = 0; /* reset flag to 0 at the beginning of each iteration */
+		for (i = 0; i < size - 1; i++) /* iterate over the array */
+		{
+			/* if current element is greater than next */
+			if (array[i] > array[i + 1])
+			{
+				/* swap current and next elements */
+				swap1(&array[i], &array[i + 1]);
+
+				flag = 1; /* set flag to 1 to indicate swap was made */
+				print_array(array, size);
+			}
+		}
+
+		if (flag == 0) /* if no swaps were made, break out of the loop */
+		break;
+	}
+}
