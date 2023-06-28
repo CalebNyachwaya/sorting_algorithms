885c6c52a04c562e23e6ff0ef4e19fdae102ce8f
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-06-27 17:05:52 -0700

    Update 2-selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,26 +10,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i, j, min_index;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
+		min_index = i;
 
 		/* Find the minimum element in unsorted array */
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
+			if (array[j] < array[min_index])
+				min_index = j;
 		}
 
 		/* Swap the found minimum element with the first unsorted element */
-		if (min_idx != i)
+		if (min_index != i)
 		{
 			int temp;
 
-			temp = array[min_idx];
-			array[min_idx] = array[i];
+			temp = array[min_index];
+			array[min_index] = array[i];
 			array[i] = temp;
 			print_array(array, size);
 		}
